9cbea74eee43b937bb22dce3ea57bc3ec992ed96
Arni Magnusson <thisisarni@gmail.com>
2026-09-24 18:15:41 -0400

    Run projections on 02_nathan_example

--- projections/projections.R
+++ projections/projections.R
@@ -1,29 +1,23 @@
-# Download model results
-if(!dir.exists("01_google_drive")){
-  options(timeout = max(300))  # don't give up on download until after 5 minutes
-  download.file("https://github.com/arni-magnusson/rs/releases/download/01_google_drive/01_google_drive.zip", "01_google_drive.zip")
-  unzip("01_google_drive.zip", exdir="01_google_drive")
-}
+# Copy model results
+file.copy("../model_runs/02_nathan_example", ".", recursive=TRUE)
 
-# Prepare executable
-Sys.chmod("01_google_drive/ss3")
+# Copy executable
+file.copy("../ss3/ss3", "02_nathan_example")
 
-# Download function
-if(!exists("run.projections")){
-  source("https://raw.githubusercontent.com/SEFSC/SFD-AllocationForecasting/refs/heads/main/Allocation_forecasting.R")
-}
+# Source
+source("Allocation_forecasting.R")
 
 # Prepare function inputs
-Assessment_dir <- "01_google_drive"
-abc_frac <- 0.75  # ABC target at 75% of the OFL F
-OFL_rec_set <- 3  # OFL catches calculated using recent mean recruitment
-ABC_rec_set <- 3  # same as for OFL
+Assessment_dir <- "02_nathan_example"
+ABC_Fraction <- 0.75      # ABC target at 75% of the OFL F
+OFL_recruit_setting <- 3  # OFL catches calculated using recent mean recruitment
+ABC_recruit_setting <- 3  # same as for OFL
 
 # For reference the Benchmark runs always use the stock-recruit curve to
 # calculate F_OFL and unfished SSB
 
 # Run function
-run.projections(Assessment_dir = Assessment_dir,
-                ABC_Fraction = abc_frac,
-                OFL_recruit_setting = OFL_rec_set,
-                ABC_recruit_setting = ABC_rec_set)
+results <- run.projections(Assessment_dir = Assessment_dir,
+                           ABC_Fraction = ABC_Fraction,
+                           OFL_recruit_setting = OFL_recruit_setting,
+                           ABC_recruit_setting = ABC_recruit_setting)
